0e0f2a0baf38a6e106b5b2760dc1896f13815719
Tanja-T-V <tanja.taiv.vali@outlook.com>
2026-08-12 12:02:06 +0200

    Database.js no longer ignored

---
+++ backend/database/database.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import { Client } from 'pg';
+// ----- PostgreSQL -------
+// For PostgreSQL. gets .env file variables
+dotenv.config();
+export const database = new Client({
+    connectionString: process.env.PGURI,
+});
+database.connect();
+//# sourceMappingURL=database.js.map
