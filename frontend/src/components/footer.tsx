5e38f7a291abb808c5bbf40e1b92257d8d94281e
Tanja-T-V <tanja.taiv.vali@outlook.com>
2026-08-14 13:11:18 +0200

    Updated footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,11 +1,38 @@
+import { Dot, CCircle } from 'react-bootstrap-icons';
+
 function Footer() {
     return (
-        <nav className="Footer">
-            <ul>
-                <li>Email: wallsofhome@test.com</li>
-                <li>Login</li>
-            </ul>
-        </nav>
+        <div className="Footer">
+            <div className="mb-4">
+                <p className="fw-bold mb-1">Walls of Home</p>
+                <p className="fw-light">Finding places that feel like home.</p>
+            </div>
+
+            <div className="footer-links d-flex align-items-center gap-2 mb-4 flex-wrap">
+                <p>Explore</p>
+                <div className="d-flex align-items-center gap-1">
+                    <Dot />
+                    <p>Sell</p>
+                </div>
+                <div className="d-flex align-items-center gap-1">
+                    <Dot />
+                    <p>About</p>
+                </div>
+                <div className="d-flex align-items-center gap-1">
+                    <Dot />
+                    <p>Contact</p>
+                </div>
+                <div className="d-flex align-items-center gap-1">
+                    <Dot />
+                    <p>Help</p>
+                </div>
+            </div>
+
+            <div className="d-flex align-items-center gap-1">
+                <CCircle />
+                <p className="m-0 fw-light">2026 Walls of Home</p>
+            </div>
+        </div>
     );
 }
 
